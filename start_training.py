6bee676152fcef39fec8b725f51f023c56957d75
satishmachine <satishkumar8055@gmail.com>
2024-08-30 17:12:46 +0530

    trainig pipeline structure updated

--- start_training.py
+++ start_training.py
@@ -3,13 +3,14 @@ import sys
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
-from networksecurity.pipeline.training_pipeline import Trainingpipeline
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 def start_training():
     try:
-        pass
+        model_training = TrainingPipeline()
+        model_training.run_pipeline()
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
 if __name__ == "__main__":
-    start_training()
+    start_training() 
