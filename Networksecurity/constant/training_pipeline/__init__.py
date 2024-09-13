fc732913037354d009d6ce2953ca6344fa92f6d1
satishmachine <satishkumar8055@gmail.com>
2024-09-13 21:45:10 +0530

    data validation is working

--- Networksecurity/constant/training_pipeline/__init__.py
+++ Networksecurity/constant/training_pipeline/__init__.py
@@ -34,7 +34,7 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 '''
 Data Validation related constant starts with DATA_VALIDATION VAR NAME
 '''
-DATA_VALIDATIONA_DIR_NAME:str = "data_validation"
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
 DATA_VALIDATION_VALID_DIR:str = "validated"
 DATA_VALIDATION_INVALID_DIR:str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
